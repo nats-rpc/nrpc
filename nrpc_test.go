f0f4ec0e616dfc9ee82c27f4ab8aea5e28e83b6b
Christophe de Vienne <christophe@cdevienne.info>
2017-10-13 16:43:47 +0200

    Generate the test DummyMessage from a .proto file
    The test package had to be renamed to nrpc_test (and not simply nrpc) to avoid a
    redefinition of fileDescriptor0

--- nrpc_test.go
+++ nrpc_test.go
@@ -1,4 +1,4 @@
-package nrpc
+package nrpc_test
 
 import (
 	"bytes"
@@ -8,7 +8,12 @@ import (
 
 	"github.com/golang/protobuf/proto"
 	nats "github.com/nats-io/go-nats"
+
+	"github.com/rapidloop/nrpc"
 )
+
+//go:generate protoc --go_out=. nrpc_test.proto
+//go:generate mv nrpc_test.pb.go nrpcpb_test.go
 
 func TestBasic(t *testing.T) {
 	nc, err := nats.Connect(nats.DefaultURL, nats.Timeout(5*time.Second))
@@ -18,7 +23,7 @@ func TestBasic(t *testing.T) {
 	defer nc.Close()
 
 	subn, err := nc.Subscribe("foo.*", func(m *nats.Msg) {
-		if err := Publish(&DummyMessage{"world"}, "", nc, m.Reply, "protobuf"); err != nil {
+		if err := nrpc.Publish(&DummyMessage{"world"}, "", nc, m.Reply, "protobuf"); err != nil {
 			t.Fatal(err)
 		}
 	})
@@ -28,7 +33,7 @@ func TestBasic(t *testing.T) {
 	defer subn.Unsubscribe()
 
 	var dm DummyMessage
-	if err := Call(
+	if err := nrpc.Call(
 		&DummyMessage{"hello"}, &dm, nc, "foo.bar", "protobuf", 5*time.Second,
 	); err != nil {
 		t.Fatal(err)
@@ -55,7 +60,7 @@ func TestDecode(t *testing.T) {
 			t.Fatal(err)
 		} else if dm.Foobar != "hello" {
 			t.Fatal("unexpected inner request: " + dm.Foobar)
-		} else if err := Publish(&DummyMessage{"world"}, "", nc, m.Reply, "protobuf"); err != nil {
+		} else if err := nrpc.Publish(&DummyMessage{"world"}, "", nc, m.Reply, "protobuf"); err != nil {
 			t.Fatal(err)
 		}
 	})
@@ -68,7 +73,7 @@ func TestDecode(t *testing.T) {
 	for _, n := range names {
 		name = n
 		var dm DummyMessage
-		if err := Call(
+		if err := nrpc.Call(
 			&DummyMessage{"hello"}, &dm, nc, "foo."+name, "protobuf", 5*time.Second,
 		); err != nil {
 			t.Fatal(err)
@@ -87,7 +92,7 @@ func TestError(t *testing.T) {
 	defer nc.Close()
 
 	subn, err := nc.Subscribe("foo.*", func(m *nats.Msg) {
-		if err := Publish(&DummyMessage{"world"}, "anerror", nc, m.Reply, "protobuf"); err != nil {
+		if err := nrpc.Publish(&DummyMessage{"world"}, "anerror", nc, m.Reply, "protobuf"); err != nil {
 			t.Fatal(err)
 		}
 	})
@@ -96,7 +101,7 @@ func TestError(t *testing.T) {
 	}
 	defer subn.Unsubscribe()
 
-	err = Call(&DummyMessage{"hello"}, &DummyMessage{}, nc, "foo.bar", "protobuf", 5*time.Second)
+	err = nrpc.Call(&DummyMessage{"hello"}, &DummyMessage{}, nc, "foo.bar", "protobuf", 5*time.Second)
 	if err == nil {
 		t.Fatal("error expected")
 	}
@@ -114,7 +119,7 @@ func TestTimeout(t *testing.T) {
 
 	subn, err := nc.Subscribe("foo.*", func(m *nats.Msg) {
 		time.Sleep(time.Second)
-		if err := Publish(&DummyMessage{"world"}, "", nc, m.Reply, "protobuf"); err != nil {
+		if err := nrpc.Publish(&DummyMessage{"world"}, "", nc, m.Reply, "protobuf"); err != nil {
 			t.Fatal(err)
 		}
 	})
@@ -123,7 +128,7 @@ func TestTimeout(t *testing.T) {
 	}
 	defer subn.Unsubscribe()
 
-	err = Call(&DummyMessage{"hello"}, &DummyMessage{}, nc, "foo.bar", "protobuf", 500*time.Millisecond)
+	err = nrpc.Call(&DummyMessage{"hello"}, &DummyMessage{}, nc, "foo.bar", "protobuf", 500*time.Millisecond)
 	if err == nil {
 		t.Fatal("error expected")
 	} else if err.Error() != "nats: timeout" {
@@ -145,7 +150,7 @@ var (
 func TestMarshal(t *testing.T) {
 	for _, tt := range encodingTests {
 		t.Run("Marshal"+tt.encoding, func(t *testing.T) {
-			b, err := Marshal(tt.encoding, &encodingTestMsg)
+			b, err := nrpc.Marshal(tt.encoding, &encodingTestMsg)
 			if err != nil {
 				t.Fatal(err)
 			}
@@ -160,7 +165,7 @@ func TestUnmarshal(t *testing.T) {
 	for _, tt := range encodingTests {
 		t.Run("Unmarshal"+tt.encoding, func(t *testing.T) {
 			var msg DummyMessage
-			err := Unmarshal(tt.encoding, tt.data, &msg)
+			err := nrpc.Unmarshal(tt.encoding, tt.data, &msg)
 			if err != nil {
 				t.Fatal(err)
 			}
@@ -188,7 +193,7 @@ func TestExtractFunctionNameAndEncoding(t *testing.T) {
 		{"foo.bar.json.protobuf", "", "",
 			"Invalid subject. Expects 2 or 3 parts, got foo.bar.json.protobuf"},
 	} {
-		name, encoding, err := ExtractFunctionNameAndEncoding(tt.subject)
+		name, encoding, err := nrpc.ExtractFunctionNameAndEncoding(tt.subject)
 		if name != tt.name {
 			t.Errorf("Expected name=%s, got %s", tt.name, name)
 		}
@@ -203,28 +208,4 @@ func TestExtractFunctionNameAndEncoding(t *testing.T) {
 			t.Errorf("Expected error '%s', got '%s'", tt.err, err)
 		}
 	}
-}
-
-//------------------------------------------------------------------------------
-
-type DummyMessage struct {
-	Foobar string `protobuf:"bytes,1,opt,name=foobar" json:"foobar,omitempty"`
-}
-
-func (m *DummyMessage) Reset()                    { *m = DummyMessage{} }
-func (m *DummyMessage) String() string            { return proto.CompactTextString(m) }
-func (*DummyMessage) ProtoMessage()               {}
-func (*DummyMessage) Descriptor() ([]byte, []int) { return dummyfileDescriptor0, []int{0} }
-
-func init() {
-	proto.RegisterType((*DummyMessage)(nil), "DummyMessage")
-}
-
-var dummyfileDescriptor0 = []byte{
-	// 76 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x09, 0x6e, 0x88, 0x02, 0xff, 0xe2, 0xe2, 0xca, 0x2b, 0x2a, 0x48,
-	0xd6, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x57, 0x52, 0xe3, 0xe2, 0x71, 0x29, 0xcd, 0xcd, 0xad, 0xf4,
-	0x4d, 0x2d, 0x2e, 0x4e, 0x4c, 0x4f, 0x15, 0x12, 0xe3, 0x62, 0x4b, 0xcb, 0xcf, 0x4f, 0x4a, 0x2c,
-	0x92, 0x60, 0x54, 0x60, 0xd4, 0xe0, 0x0c, 0x82, 0xf2, 0x92, 0xd8, 0xc0, 0xca, 0x8d, 0x01, 0x01,
-	0x00, 0x00, 0xff, 0xff, 0x76, 0x6f, 0x42, 0xc1, 0x3c, 0x00, 0x00, 0x00,
 }
